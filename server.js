93f81661cc7fa802413c47461e5f95488bdc9357
Cranekick50 <michael.srithapin@gmail.com>
2020-07-13 18:25:22 -0500

    worked with tutor on adding several functions

--- server.js
+++ server.js
@@ -2,26 +2,26 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "password",
-    database: "employee_db",
-  });
-  
-  connection.connect(function (err) {
-    if (err) throw err;
-    startQuestions();
-  });
+  host: "localhost",
 
-  function startQuestions() {
-    inquirer
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "employee_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  startQuestions();
+});
+
+function startQuestions() {
+  inquirer
     .prompt({
       name: "action",
       type: "list",
@@ -71,72 +71,170 @@ var connection = mysql.createConnection({
         case "Update Employee Role":
           updateRole();
           break;
-        
+
         case "Update Employee Manager":
           updateManager();
           break;
       }
     });
-  };
+}
 
-  function viewAll() {
-    connection.query(
-      "SELECT * FROM employee_db", function(err,res) {
-        if (err) throw err;
-      console.log(res);
-      connection.end();
-      }
-    )
+function viewAll() {
+  connection.query(`SELECT employee.first_name, employee.last_name, role.salary, role.title, department.name as "department name"
+  FROM employee_db.employee
+  INNER JOIN role ON employee.role_id = role.id
+  INNER JOIN department ON role.department_id = department.id`, 
+  function (err, res) {
+    if (err) throw err;
+    console.table(res);
     startQuestions();
-  };
-  
-  function viewByDept() {
-    inquirer
-      .prompt({
-        name: "department",
-        type: "choice",
-        message: "What department?",
-        choice: []
-      })
-      .then(function (answer) {
-        console.log(answer.department);
-        connection.query(
-          "SELECT * FROM employee_db WHERE ?",
-          { department: answer.department },
-          function (err, res) {
-            if (err) throw err;
-            console.log(
-              // table columns
-            );
-            startQuestions();
-          }
-        );
-      });
+  });
+}
+
+function viewByDept() {
+  inquirer
+    .prompt({
+      name: "department",
+      type: "input",
+      message: "Which department?",
+    })
+    .then(function (answer) {
+      connection.query(
+        `SELECT employee.first_name, employee.last_name, role.salary, role.title, department.name as "department name"
+      FROM employee_db.employee
+      INNER JOIN role ON employee.role_id = role.id
+      INNER JOIN department ON role.department_id = department.id
+      WHERE department.name LIKE '${answer.department}'`,
+        function (err, res) {
+          if (err) throw err;
+          console.table(res);
+        }
+      );
+      startQuestions();
+    });
+}
 
 function viewByRole() {
-    console.log ("this works")
+  inquirer
+    .prompt({
+      name: "role",
+      type: "input",
+      message: "Which role?",
+    })
+    .then(function (answer) {
+      connection.query(
+        `SELECT employee.first_name, employee.last_name, role.salary, role.title, department.name as "department name"
+      FROM employee_db.employee
+      INNER JOIN role ON employee.role_id = role.id
+      INNER JOIN department ON role.department_id = department.id
+      WHERE role.title LIKE '${answer.role}'`,
+        function (err, res) {
+          if (err) throw err;
+          console.table(res);
+        }
+      );
+      startQuestions();
+    });
 }
 
 function createDept() {
-    console.log ("this works")
+  inquirer.prompt ({
+    name: "addDept",
+    type: "input",
+    message: "What is the name of the department?",
+  })
+  .then(function (answer) {
+    connection.query(
+      "INSERT INTO department SET ?", {name: answer.addDept}
+    )
+    startQuestions();
+  })
 }
 
 function createRole() {
-    console.log ("this works")
+  inquirer.prompt ({
+    name: "addRole",
+    type: "input",
+    message: "What is the name of the department?",
+  })
+  .then(function (answer) {
+    connection.query(
+      "INSERT INTO role SET ?", {name: answer.addRole}
+    )
+    startQuestions();
+  })
 }
-
+//investigate why department name is being added to added employees
 function addEmployee() {
-    console.log ("this works")
+  inquirer.prompt ([{
+    name: "addFirst",
+    type: "input",
+    message: "What is the first name of the employee?",
+  },
+  {
+    name: "addLast",
+    type: "input",
+    message: "What is the first name of the employee?",
+  },
+  {
+    name: "addRole",
+    type: "number",
+    message: "What is the role ID (must be a number)?",
+  },
+  {
+    name: "addManager",
+    type: "number",
+    message: "What is the manager ID (must be a number)?",
+  }
+])
+  .then(function (answer) {
+    connection.query(
+      "INSERT INTO employee SET ?", {first_name: answer.addFirst, last_name: answer.addLast, role_id: answer.addRole, manager_id: answer.addManager}
+    )
+    startQuestions();
+  })
 }
 
 function delEmployee() {
-    console.log ("this works")
+  console.log("this works");
 }
 
+//try to update the below to not be number related
 function updateRole() {
-    console.log ("this works")
+  inquirer.prompt ([{
+    name: "employee_id",
+    type: "input",
+    message: "Who do you want to update?",
+  },
+  {
+    name: "role_id",
+    type: "number",
+    message: "What is the new role ID (must be a number)?",
+  }])
+  .then(function (answer) {
+    connection.query(
+      "UPDATE employee SET role_id= ? WHERE id= ?", [answer.role_id, answer.employee_id]
+    )
+  })
+  startQuestions();
 }
+
 
 function updateManager() {
-    console.log ("this works")
+  inquirer.prompt ([{
+    name: "employee_id",
+    type: "input",
+    message: "Who do you want to update?",
+  },
+  {
+    name: "manager_id",
+    type: "number",
+    message: "What is the new role ID (must be a number)?",
+  }])
+  .then(function (answer) {
+    connection.query(
+      "UPDATE employee SET role_id= ? WHERE id= ?", [answer.manager_id, answer.employee_id]
+    )
+  })
+  startQuestions();
 }
